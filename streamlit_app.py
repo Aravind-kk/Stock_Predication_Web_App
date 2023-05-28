f1b6687ac1121665e435601c83e8e611f47bcceb
Aravind-k <120537950+Aravind-kk@users.noreply.github.com>
2023-05-29 01:22:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Predication Web App')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'AMZN', 'FB', 'TSLA', 'MA', 'PYPL')
+stocks = ('GOOG', 'AAPL', 'MSFT', 'AMZN', 'NFLX', 'TSLA', 'MA', 'PYPL')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
